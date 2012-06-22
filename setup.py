61ec1d7dc130e85323d8c5d5abc161387f48571c
Ib Lundgren <ib.lundgren@gmail.com>
2012-06-22 14:25:02 +0200

    Basic setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from os.path import dirname, join
+from setuptools import setup, find_packages
+
+
+def fread(fn):
+    with open(join(dirname(__file__), fn), 'r') as f:
+        return f.read()
+
+tests_require = ['nose', 'unittest2']
+
+requires = []
+
+setup(
+    name='flask-oauthprovider',
+    version='0.1.0',
+    description='A full featured and secure OAuth provider base',
+    long_description=fread('README.rst'),
+    author='Ib Lundgren',
+    author_email='ib.lundgren@gmail.com',
+    url='https://github.com/ib-lundgren/flask-oauthprovider',
+    license=fread('LICENSE'),
+    packages=find_packages(exclude=('docs')),
+    test_suite='nose.collector',
+    tests_require=tests_require,
+    extras_require={'test': tests_require},
+    install_requires=requires,
+)
