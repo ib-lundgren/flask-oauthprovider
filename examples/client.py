285821ef6bc8c99ed6053147fad90d81cb3792ee
Ib Lundgren <ib.lundgren@gmail.com>
2012-06-26 17:26:09 +0200

    fixed client cookie saving issue

--- examples/client.py
+++ examples/client.py
@@ -4,20 +4,25 @@ from flask import Flask, redirect, request, session
 from urlparse import parse_qsl, urlparse
 
 app = Flask(__name__)
-app.secret_key = "not very secret"
+app.config.update(
+    SECRET_KEY = "not very secret",
+    SERVER_NAME = "client.local:5001"
+)
 # TODO: read client key + secret from some config
 
 
 @app.route("/start")
 def start():
-    client = OAuth1(u"gYMw03rEFMmaK2g18usXfpUw5awjcH", 
-        client_secret=u"AUMLINSKDHwWJqwvVp7u1DmTznQhhV",
-        callback_uri=u"http://127.0.0.1:5001/callback")
+    client = OAuth1(u"sHcpSA23dd7ntT6BDVqU3FmrvaeAK2", 
+        client_secret=u"pKtlX5NqIA7qGlJanHMH6rvyxZKRvD",
+        callback_uri=u"http://client.local:5001/callback")
     r = requests.post(u"http://127.0.0.1:5000/request_token?realm=secret", auth=client)
     print r.content
     data = dict(parse_qsl(r.content))
     resource_owner = data.get(u'oauth_token')
     session["token_secret"] = data.get('oauth_token_secret').decode(u'utf-8')
+    print "Setting token secret"
+    print session
     # TODO: store secret
     url = u"http://127.0.0.1:5000/authorize?oauth_token=" + resource_owner
     return redirect(url)
@@ -29,12 +34,14 @@ def callback():
     data = dict(parse_qsl(urlparse(request.url).query))
     resource_owner = data.get(u'oauth_token').decode(u'utf-8')
     verifier = data.get(u'oauth_verifier').decode(u'utf-8')
+    print session
+    print "Secret gathered"
     token_secret = session["token_secret"]
 
     # Request the access token
     # TODO: use secret when fetching access token
-    client = OAuth1(u"gYMw03rEFMmaK2g18usXfpUw5awjcH",
-        client_secret=u"AUMLINSKDHwWJqwvVp7u1DmTznQhhV",
+    client = OAuth1(u"sHcpSA23dd7ntT6BDVqU3FmrvaeAK2",
+        client_secret=u"pKtlX5NqIA7qGlJanHMH6rvyxZKRvD",
         resource_owner_key=resource_owner,
         resource_owner_secret=token_secret,
         verifier=verifier)
@@ -46,8 +53,8 @@ def callback():
     resource_owner = data.get(u'oauth_token').decode(u'utf-8')
     resource_owner_secret = data.get(u'oauth_token_secret').decode(u'utf-8')
     print data
-    client = OAuth1(u"gYMw03rEFMmaK2g18usXfpUw5awjcH",
-            client_secret=u"AUMLINSKDHwWJqwvVp7u1DmTznQhhV",
+    client = OAuth1(u"sHcpSA23dd7ntT6BDVqU3FmrvaeAK2",
+            client_secret=u"pKtlX5NqIA7qGlJanHMH6rvyxZKRvD",
             resource_owner_key=resource_owner,
             resource_owner_secret=resource_owner_secret)
     print "SUPER SECRET::", resource_owner_secret
