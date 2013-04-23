cf150ed4e561337bb9fd445710b34795a3e2baaa
David McKeone <david@artsman.com>
2013-04-23 07:54:31 -0600

    Change signature of save_request_token() on comment as well

--- flask_oauthprovider.py
+++ flask_oauthprovider.py
@@ -26,7 +26,7 @@ class OAuthProvider(Server):
             request_token=None, access_token=None
     * authorize(self)
     * get_callback(self, request_token)
-    * save_request_token(self, client_key, request_token, realm=None,
+    * save_request_token(self, client_key, request_token, callback, realm=None,
             secret=None)
     * save_verifier(self, client_key, request_token, verifier)
     * save_access_token(self, client_key, request_token, realm=None,
